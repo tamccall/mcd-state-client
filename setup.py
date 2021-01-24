1aac667fad55cae772dbedc3aadb37ea05e03ae5
tamccall <andymc903@gmail.com>
2021-01-23 19:04:47 -0800

    reformatted with black

--- setup.py
+++ setup.py
@@ -13,5 +13,5 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/tamccall/mcd-state-client",
     packages=setuptools.find_packages(),
-    python_requires='>=3.6',
+    python_requires=">=3.6",
 )
--- tests/example.py
+++ tests/example.py
@@ -6,5 +6,5 @@ class MyTestCase(unittest.TestCase):
         self.assertEqual(True, True)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     unittest.main()
